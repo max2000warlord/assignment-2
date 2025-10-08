3698ce1a3a51bf7c4c3cab30d427cc73473f9cc4
Matthew Anderson <mpa-business@pm.me>
2025-10-09 01:56:16 +1100

    deploying

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'export', // Enables static export for Next.js 13+
   turbopack: {
     root: __dirname, // explicitly set your project root
   },
-}
-
-module.exports = {
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
   eslint: {
     ignoreDuringBuilds: true,
