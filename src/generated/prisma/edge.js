3966caacc9965b995711001650d93ae1fcd6ec08
Matthew Anderson <mpa-business@pm.me>
2025-10-16 06:30:54 +1100

    tweaks

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -143,7 +143,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
